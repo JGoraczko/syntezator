acac1a2a08d78cc3e2d434c9bf948211f3929e3d
LukaszReszka <74792443+LukaszReszka@users.noreply.github.com>
2021-05-08 23:15:17 +0200

    Add files via upload

--- ProcessC/main.cpp
+++ ProcessC/main.cpp
@@ -22,15 +22,38 @@ short SineWave (double time, double freq, const int SAMPLE_RATE)
     return amplitude;
 }
 
+short SquareWave (double time, double freq, const int SAMPLE_RATE)
+{
+    short amplitude = 0;
+    int ticks_per_cycle = SAMPLE_RATE/freq;
+    int cycle_part = int(time)%ticks_per_cycle;
+    int half_cycle = ticks_per_cycle/2;
+    if (cycle_part < half_cycle) amplitude = 32767;
+    return amplitude;
+}
+
+short SawWave (double time, double freq, const int SAMPLE_RATE)
+{
+    double ticks_per_cycle = SAMPLE_RATE/freq;
+    short amplitude = 32767;
+    amplitude *= 2*((time/ticks_per_cycle) - floor(0.5+time/ticks_per_cycle));
+    return amplitude;
+}
+
+short TriangleWave (double time, double freq, const int SAMPLE_RATE)
+{
+    return 2*abs(SawWave(time, freq, SAMPLE_RATE))-1;
+}
+
 int main()
 {
     const int SAMPLE_RATE = 44100;
     sf::SoundBuffer buffer, buffer2;
     std::vector<sf::Int16> paczka, paczka2;
     for (int s = 0; s < 44100; ++s)
-        paczka.push_back(SineWave(s, 440, SAMPLE_RATE));
+        paczka.push_back(TriangleWave(s, 440, SAMPLE_RATE));
     for (int s = 0; s < 44100; ++s)
-        paczka2.push_back(SineWave(s, 880, SAMPLE_RATE));
+        paczka2.push_back(SawWave(s, 440, SAMPLE_RATE));
     buffer.loadFromSamples(&paczka[0], paczka.size(), 1, SAMPLE_RATE);
     MusicPlayer player;
     player.load(buffer);
